e036f36eba9099fc579d5433038ed94a5b25715a
Adele_D <elmaadeledube@gmail.com>
2022-09-14 17:11:48 +0300

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <p className="footer">
+          <a
+            href="https://github.com/adele-dube/react_js_weather_app-2022"
+            id="footer-link"
+            target="_blank"
+          >
+            Open-Source Code_
+          </a>
+          by Adele_D ©2022
+        </p>
+      </div>
+    </div>
+  );
 }
